771785673c12dd483ef8309d80ad6087a6897c67
phamtuan162 <tnpham352@gmail.com>
2024-09-21 11:56:45 +0700

    update code

--- frontend-nextjs/src/app/settings/_components/FormUpdateProfile.js
+++ frontend-nextjs/src/app/settings/_components/FormUpdateProfile.js
@@ -8,7 +8,6 @@ import FormUpdateAvatar from "./FormUpdateAvatar";
 import { toast } from "react-toastify";
 const { updateUser } = userSlice.actions;
 const FormUpdateProfile = ({ user }) => {
-  const int = 8;
   const dispatch = useDispatch();
   const nameRef = useRef(null);
   const [form, setForm] = useState({
